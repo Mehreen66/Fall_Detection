8b7e586907d2552683c25fa13ae8852887d9d7c9
Hassaan <hassaan.butt9999@outlook.com>
2020-05-20 21:23:36 +0500

    Adapter created.

---
+++ app/src/main/java/com/example/project/MyAdapter.java
@@ -0,0 +1,41 @@
+package com.example.project;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private String[] data;
+    public MyAdapter(String[] Data)
+    {
+        data = Data;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        //View view = inflater.inflate(R.layout.);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
